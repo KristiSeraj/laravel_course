6da7e4b9326c53a852352f1d787e5cca382578ec
KristiSeraj <serajkristi@gmail.com>
2024-12-09 21:53:42 +0100

    parameters in routes, dynamic routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,4 @@
     <x-slot:heading>
         Homepage
     </x-slot:heading>
-
-    @foreach($jobs as $job)
-        <li><strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.</li>
-    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', [
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
         'jobs' => [
             [
+                'id' => 1,
                 'title' => 'Director',
                 'salary' => '$50,000'
             ],
             [
+                'id' => 2,
                 'title' => 'Programmer',
                 'salary' => '$100,000'
             ],
             [
+                'id' => 3,
                 'title' => 'Teacher',
                 'salary' => '$40,000'
             ]
@@ -21,8 +29,27 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$100,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$40,000'
+        ]
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
