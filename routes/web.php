1220626c91314c7a8f1405ef56d91f76aac9610d
KristiSeraj <serajkristi@gmail.com>
2024-12-10 21:34:11 +0100

    404 page, autoloading, models

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,7 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$100,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
-}
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -35,8 +12,7 @@ Route::get('/jobs', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-    return view('job', ['job' => $job]);
+    return view('job', ['job' => Job::find($id)]);
 });
 
 Route::get('/contact', function () {
