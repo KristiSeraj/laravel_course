a3f90361df8359b0ae08349ac6f5176f465d7fdc
KristiSeraj <serajkristi@gmail.com>
2024-12-10 20:57:17 +0100

    array transformed to class

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,10 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
+class Job {
+    public static function all(): array
+    {
+        return [
             [
                 'id' => 1,
                 'title' => 'Director',
@@ -25,30 +22,20 @@ Route::get('/jobs', function () {
                 'title' => 'Teacher',
                 'salary' => '$40,000'
             ]
-        ]
-    ]);
+        ];
+    }
+}
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$100,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
 
